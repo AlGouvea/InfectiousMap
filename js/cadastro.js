d300841eb8885fe60ffb8d4923847a3831956211
AlGouvea <alvarohsgouvea@gmail.com>
2020-11-12 18:48:55 -0300

    Comentado o codigo de usuario do db para poder ser testado depois

--- js/cadastro.js
+++ js/cadastro.js
@@ -1,22 +1,22 @@
 //Documento de formatacao do objeto pro database
 // const database = require('./db')
 
-const usuario = database.sequelize.define('usuarios', {
-    User: {
-        type: database.sequelize.STRING
-    },
-    RA: {
-        type: database.sequelize.INTEGER
-    },
-    Tipo: {
-        type: database.sequelize.INTEGER
-    },
-    CRM: {
-        type: database.sequelize.STRING
-    },
-    Senha: {
-        type: database.sequelize.STRING
-    }
-})
+// const usuario = database.sequelize.define('usuarios', {
+//     User: {
+//         type: database.sequelize.STRING
+//     },
+//     RA: {
+//         type: database.sequelize.INTEGER
+//     },
+//     Tipo: {
+//         type: database.sequelize.INTEGER
+//     },
+//     CRM: {
+//         type: database.sequelize.STRING
+//     },
+//     Senha: {
+//         type: database.sequelize.STRING
+//     }
+// })
 
-module.exports = usuario
+// module.exports = usuario
